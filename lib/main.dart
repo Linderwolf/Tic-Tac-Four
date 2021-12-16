7644ff09a5b8c0e186fe532c048610305b32be3f
Linderwolf <91448628+Linderwolf@users.noreply.github.com>
2021-12-16 17:11:46 -0500

    theme and tictactoe button

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import './tictactoe.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    theme: ThemeData(
+      brightness:Brightness.dark,
+      primarySwatch: Colors.deepPurple,
+      primaryColor: const Color(0xFF212121),
+      accentColor: const Color(0xFF64ffda),
+      canvasColor: const Color(0xFF303030),
+      fontFamily: 'Roboto',
+    ),
+  ),);
+}
 
 final ThemeData themeData = ThemeData(
   canvasColor: Colors.black,
 );
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext ctx){
     return Scaffold(
@@ -23,4 +36,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TicTacToe e
+
+
+// class TicTacToe extends MaterialPageRoute<Null> {
+//   TicTacToe() : super(builder: (BuildContext ctx)){
+//     return Scaffold(
+//
+//     );
+//   });
+// }
